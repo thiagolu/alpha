76305fe8e1af3b2c23ea96b63d18c0d0801f30fe
Thiago Lu <tlusilva05@gmail.com>
2016-01-18 22:25:58 -0200

    edit articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+	before_action :find_model, only: [ :show, :edit, :update ]
+
+	def index
+		@articles = Article.all		
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+		render 'new'
+	end
+
+	def show
+	end
+
+	def update
+		if @article.update(article_params)
+			flash[:notice] =  "The Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def create
+		@article = Article.new(article_params)	
+		if @article.save
+			flash[:notice] = "The Article was created."
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end	
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+
+	def find_model
+		@article = Article.find(params[:id]) if params[:id]
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'index', to: 'welcome#index'
   get 'about', to: 'welcome#about'
 
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
