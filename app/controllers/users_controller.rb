b932c9f09709b8eb2c5f308fc4920e4dc9159ed1
Thiago Lu <tlusilva05@gmail.com>
2016-01-31 20:06:05 -0200

    edit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success] = "Welcom #{@user.username}"
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		find_user
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:success] = "Your account was updated"
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)		
+	end
+
+	def find_user
+		@user = Users.find(params[:id]) if params[:id]
+	end
+end
