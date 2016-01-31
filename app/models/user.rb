d33326f45be85c46fc120ba7fd779296db6e8066
Thiago Lu <tlusilva05@gmail.com>
2016-01-31 18:28:51 -0200

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class User < ActiveRecod::Base
+class User < ActiveRecord::Base
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :username, presence: true,
+											 uniqueness: { case_sensitive: false },
+											 length: { minimum: 3, maximum: 20 }
+
+	validates :email, presence: true,
+										uniqueness: { case_sensitive: false },
+										format: { with: VALID_EMAIL_REGEX }
 end
