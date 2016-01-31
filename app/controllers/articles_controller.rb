43bccdb9dd7cac99bf592d275952279aeabd1965
Thiago Lu <tlusilva05@gmail.com>
2016-01-31 18:54:49 -0200

    hard code association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)	
+		@article.user = User.first
 		if @article.save
 			flash[:success] = "The Article was created."
 			redirect_to article_path(@article)
