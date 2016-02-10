ac2a3aabdb6ab7cadfe2853eeb9e92a5015009ed
Thiago Lu <tlusilva05@gmail.com>
2016-02-10 20:22:35 -0200

    porra muita coisa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,4 +8,8 @@ class WelcomeController < ApplicationController
 		
 	end
 
+	def header
+		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  get 'header', to: 'welcome#header'
   resources :users, except: 'new'
   resources :articles
 
