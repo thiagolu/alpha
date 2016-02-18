c62c2e75c3fb658a7a2627b1005da499eb9b596c
Thiago Lu <tlusilva05@gmail.com>
2016-02-18 21:47:26 -0200

    association cat/art

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
+
+	has_many :article_categories
+	has_many :articles, through: :article_categories
+
 	validates :category_name, presence: true, length: { maximum: 30, minimum: 3 }
 	validates_uniqueness_of :category_name
 end
