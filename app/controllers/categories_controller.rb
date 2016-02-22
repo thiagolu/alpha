33738634bde3897e289322f50ba889c7ec957936
Thiago Lu <tlusilva05@gmail.com>
2016-02-21 23:44:02 -0300

    show and update category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,7 @@ class CategoriesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page: params[:page], per_page: 4)
 	end
 
 	private
