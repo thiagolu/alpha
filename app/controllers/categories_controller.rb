138a57dd8f2a8d4e00d97b8bd1312fcaf1555948
Thiago Lu <tlusilva05@gmail.com>
2016-03-06 12:36:24 -0300

    css and style :lipstick:

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
 	before_action :find_category, only: [ :edit, :update, :show ]
 
 	def index
-		@categories = Category.paginate(page: params[:page], per_page: 5)
+		@categories = Category.all
 	end
 
 	def new
